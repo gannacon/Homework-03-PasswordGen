5d221cadbdbd585ed94c1d144579c93d987e03ea
gannacon <cwgannaway@gmail.com>
2021-03-27 14:45:36 -0500

    generating uppercase letters

--- script.js
+++ script.js
@@ -12,14 +12,31 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
 //create a function of generate password
-function generatePassword(){
-  var password = ' ';
-
-  var specialChars = '!@#$%^&*()_+';
-  console.log(specialChars[1])
-  return password;
+function getUppercaseLetters(){
+  var upperCaseLetters = [];
+  for (var i = 65; i < 91; i++) {
+    upperCaseLetters.push(String.fromCharCode(i));
+  }
+  return upperCaseLetters;
 }
+
+
+function generatePassword(){
+  //TODO: Define characters we will use
+  var upperCaseLetters = getUppercaseLetters();
+  console.log("Uppercase Letters ", upperCaseLetters);
+}
+getUppercaseLetters();
+generatePassword();
+  // var password = ' ';
+
+  // var specialChars = '!@#$%^&*()_+';
+  // console.log(specialChars[1]) //array notation for strings is valid
+  // return password;
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
